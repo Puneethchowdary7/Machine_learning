47d3775fa6f7eb41f50e2966451905b60e8524b7
puneeth <bnpuneeth7@gmail.com>
2022-03-30 20:37:00 +0530

    added main.py and requirements file of face recognition

---
+++ ML/main.py
@@ -0,0 +1,19 @@
+from tkinter import * # Used to create the GUI
+from tkinter import ttk # This module contains widgets
+import os, sys
+from PIL import Image # This module is used to deal with images
+
+# 
+class Face_recognition_system: 
+    def __init__(self,root):
+        self.root = root
+        self.root.geometry("1280x800")
+        self.root.title("Face Recogniton System")
+
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    obj = Face_recognition_system(root)
+    root.mainloop()
